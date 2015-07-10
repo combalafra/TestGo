222cad2507fad28043e3b573ef4ddd535b7bf6c4
combalafra <combalafra@yahoo.fr>
2015-07-10 12:48:12 +0200

    new printing done

--- TestGo/src/combalafra/test.go
+++ TestGo/src/combalafra/test.go
@@ -4,7 +4,5 @@ import "fmt"
 
 func main() {
     fmt.Printf("hello, world\n")
-    fmt.Printf("added in local repository\n")
-    fmt.Printf("remote update 2\n")
-    fmt.Printf("new try\n")
+    fmt.Printf("This is the new printing feature\n")
 }
