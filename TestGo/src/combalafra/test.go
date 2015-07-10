eff866bdef495efbeb2060294b585aff6454470c
combalafra <combalafra@yahoo.fr>
2015-07-10 12:57:31 +0200

    fix release bug 1

--- TestGo/src/combalafra/test.go
+++ TestGo/src/combalafra/test.go
@@ -5,5 +5,5 @@ import "fmt"
 func main() {
     fmt.Printf("hello, world\n")
     fmt.Printf("This is the new printing feature\n")
-    fmt.Printf("Second feature\n")
+    fmt.Printf("Second feature 2\n")
 }
